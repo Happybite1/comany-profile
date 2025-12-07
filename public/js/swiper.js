46eab0bdff06f7a394fae838fa0257bb0c32878f
Happybite1 <Happybite1>
2025-12-07 17:40:55 +0700

    tes

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -3,14 +3,23 @@ let swiper;
 function initSwiper() {
     const screenWidth = window.innerWidth;
 
+    // Aktifkan Swiper hanya jika layar ≥ 768px
     if (screenWidth >= 768 && !swiper) {
         swiper = new Swiper(".mySwiper", {
             slidesPerView: 2,
             spaceBetween: 20,
+            loop: true, // mengulang tanpa henti
+
+            autoplay: {
+                delay: 5000, // 5 detik
+                disableOnInteraction: false, // meski tombol di klik, tetap autoplay
+            },
+
             navigation: {
                 nextEl: ".slider-next",
                 prevEl: ".slider-prev",
             },
+
             breakpoints: {
                 992: { slidesPerView: 3 }
             }
@@ -24,6 +33,5 @@ function initSwiper() {
     }
 }
 
-// Jalankan saat page load & saat resize
 window.addEventListener("load", initSwiper);
 window.addEventListener("resize", initSwiper);
